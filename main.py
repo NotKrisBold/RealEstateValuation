994a8f92de25f867b350b31c9e429c662a30766a
KrisBold12 <kristianboldini@gmail.com>
2024-04-25 01:29:03 +0200

    primi passi

---
+++ main.py
@@ -0,0 +1,43 @@
+from ucimlrepo import fetch_ucirepo
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+# fetch dataset
+real_estate_valuation = fetch_ucirepo(id=477)
+
+# Inizializzazione del database
+X = pd.DataFrame(real_estate_valuation.data.features, columns=real_estate_valuation.feature_names)
+y = pd.Series(real_estate_valuation.data.targets.values.flatten(), name='house price of unit area')
+
+# Training set e test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Modello di regressione lineare
+model = LinearRegression()
+
+# Addestramento del modello sul training set
+model.fit(X_train, y_train)
+
+# Valutazione del modello sul test set
+y_pred = model.predict(X_test)
+
+# Metriche di valutazione
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+r2 = r2_score(y_test, y_pred)
+print("RMSE:", rmse)
+print("R²:", r2)
+
+# Coefficienti modello
+coefficients = pd.DataFrame({"Feature": X.columns, "Coefficient": model.coef_})
+print(coefficients)
+
+# Grafico residui
+residuals = y_test - y_pred
+plt.scatter(y_pred, residuals)
+plt.xlabel("Predicted Values (10,000 New Taiwan Dollar/Ping)")
+plt.ylabel("Residuals")
+plt.title("Residual Plot")
+plt.show()
---
+++ visualizer.py
@@ -0,0 +1,35 @@
+from ucimlrepo import fetch_ucirepo
+import pandas as pd
+
+# Fetch del dataset
+real_estate_valuation = fetch_ucirepo(id=477)
+
+df_features = pd.DataFrame(real_estate_valuation.data.features, columns=real_estate_valuation.feature_names)
+df_targets = pd.DataFrame(real_estate_valuation.data.targets, columns=real_estate_valuation.target_names)
+
+units_features = {
+    'X1 transaction date': 'anno.mese (es. 2013.250 = 2013 marzo)',
+    'X2 house age': 'anni',
+    'X3 distance to the nearest MRT station': 'metri',
+    'X4 number of convenience stores': 'unità',
+    'X5 latitude': 'gradi',
+    'X6 longitude': 'gradi'
+}
+
+units_target = {
+    'house price of unit area': '10,000 New Taiwan Dollar/Ping'
+}
+
+# Features
+print("DataFrame delle Features:")
+print(df_features.head())
+print("\nUnità di misura delle Features:")
+for feature, unit in units_features.items():
+    print(f"{feature}: {unit}")
+
+# Targets
+print("\nDataFrame delle Targets:")
+print(df_targets.head())
+print("\nUnità di misura della Target:")
+for target, unit in units_target.items():
+    print(f"{target}: {unit}")
